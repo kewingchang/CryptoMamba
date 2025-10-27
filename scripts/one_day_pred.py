6f2a1bba098678a85ad99019a7369215a4b33727
ToDoMoon <kewingchang@gmail.com>
2025-10-27 19:24:57 +0800

    Update one_day_pred.py

--- scripts/one_day_pred.py
+++ scripts/one_day_pred.py
@@ -170,8 +170,8 @@ if __name__ == "__main__":
     start_ts = end_ts - model.window_size * 24 * 60 * 60
     data = data[data['Timestamp'] >= start_ts - 60 * 60]
     data = data.tail(model.window_size)  # 添加：裁剪到最后 window_size 行
-    if len(data) < model.window_size:
-        raise ValueError(f"Data length {len(data)} < window_size = {model.window_size}, insufficient data")
+    # if len(data) < model.window_size:
+    #     raise ValueError(f"Data length {len(data)} < window_size = {model.window_size}, insufficient data")
     txt_file = init_dirs(args, pred_date)
     
     
