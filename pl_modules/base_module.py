ff25397e26c11f4af11291398c2412889ce07821
ToDoMoon <kewingchang@gmail.com>
2025-10-28 09:58:32 +0800

    Update base_module.py

--- pl_modules/base_module.py
+++ pl_modules/base_module.py
@@ -60,10 +60,10 @@ class BaseModule(pl.LightningModule):
 
     def __init__(
         self,
-        lr=0.0002, 
+        lr=0.0002,
         lr_step_size=50,
         lr_gamma=0.1,
-        weight_decay=0.0, 
+        weight_decay=0.0,
         logger_type=None,
         window_size=14,
         y_key='Close',
@@ -78,20 +78,23 @@ class BaseModule(pl.LightningModule):
         self.lr_step_size = lr_step_size
         self.lr_gamma = lr_gamma
         self.weight_decay = weight_decay
-        self.logger_type = logger_type 
+        self.logger_type = logger_type
         self.y_key = y_key
         self.optimizer = optimizer
-        self.batch_size = None   
+        self.batch_size = None
         self.mode = mode
         self.window_size = window_size
         self.loss = loss
-        self.use_revin = use_revin  # ADDED FOR REVIN
-        self.target_idx = None  # ADDED FOR REVIN: set later in training/eval/pred
+        self.use_revin = use_revin          # ADDED FOR REVIN
+        self.target_idx = None              # ADDED FOR REVIN: set later
         self.mse = nn.MSELoss()
         self.l1 = nn.L1Loss()
         self.mape = MAPE()
         self.normalization_coeffs = None
 
+    # --------------------------------------------------------------------- #
+    #  forward
+    # --------------------------------------------------------------------- #
     def forward(self, x, y_old=None):
         # ADDED FOR REVIN: apply RevIN normalization if enabled
         if self.use_revin:
@@ -100,22 +103,28 @@ class BaseModule(pl.LightningModule):
             x = rearrange(x, 'b s f -> b f s')
         # END ADDED FOR REVIN
 
-        if self.mode == 'default':
-            y_hat_norm = self.model(x).reshape(-1)
-        elif self.mode == 'diff':
-            y_hat_norm = self.model(x).reshape(-1) + y_old
+        # MODIFIED FOR REVIN: model now outputs (b, num_features, 1)
+        model_out = self.model(x)                     # (b, f, 1)
+        model_out = model_out.squeeze(-1)             # (b, f)
+        y_hat_norm = model_out[:, self.target_idx]     # (b,)
+
+        if self.mode == 'diff':
+            y_hat_norm = y_hat_norm + y_old
 
         # ADDED FOR REVIN: denormalize if use_revin
         if self.use_revin:
             y_hat = self.revin(y_hat_norm, 'denorm', target_idx=self.target_idx)
         else:
             y_hat = y_hat_norm
-        # For training, return both for loss calculation
+        # END ADDED FOR REVIN
+
         if self.training and self.use_revin:
             return y_hat, y_hat_norm
         return y_hat
-        # END ADDED FOR REVIN
 
+    # --------------------------------------------------------------------- #
+    #  other helpers
+    # --------------------------------------------------------------------- #
     def set_normalization_coeffs(self, factors):
         if factors is None:
             return
@@ -130,6 +139,9 @@ class BaseModule(pl.LightningModule):
             y_hat = y_hat * scale + shift
         return y, y_hat
 
+    # --------------------------------------------------------------------- #
+    #  training / validation / test steps
+    # --------------------------------------------------------------------- #
     def training_step(self, batch, batch_idx):
         x = batch['features']
         y = batch[self.y_key]
@@ -137,18 +149,20 @@ class BaseModule(pl.LightningModule):
         if self.batch_size is None:
             self.batch_size = x.shape[0]
 
-        # MODIFIED FOR REVIN: handle loss on normalized space if use_revin
         if self.use_revin:
             y_hat, y_hat_norm = self.forward(x, y_old)
+
             mean = self.revin.mean[:, 0, self.target_idx]
             stdev = self.revin.stdev[:, 0, self.target_idx]
             y_trans = (y - mean) / (stdev + self.revin.eps)
+
             if self.revin.affine:
                 w = self.revin.weight[self.target_idx]
                 b = self.revin.bias[self.target_idx]
                 y_norm = y_trans * w + b
             else:
                 y_norm = y_trans
+
             mse_norm = self.mse(y_hat_norm, y_norm)
             mse = self.mse(y_hat, y)
             rmse = torch.sqrt(mse)
@@ -156,18 +170,20 @@ class BaseModule(pl.LightningModule):
             l1 = self.l1(y_hat, y)
             loss = mse_norm
         else:
-            y_hat = self.forward(x, y_old).reshape(-1)
+            y_hat = self.forward(x, y_old)
             y, y_hat = self.denormalize(y, y_hat)
             mse = self.mse(y_hat, y)
             rmse = torch.sqrt(mse)
             mape = self.mape(y_hat, y)
             l1 = self.l1(y_hat, y)
             loss = mse
-        # END MODIFIED FOR REVIN
 
-        self.log("train/mse", mse.detach(), batch_size=self.batch_size, sync_dist=True, prog_bar=False)
-        self.log("train/rmse", rmse.detach(), batch_size=self.batch_size, sync_dist=True, prog_bar=True)
-        self.log("train/mape", mape.detach(), batch_size=self.batch_size, sync_dist=True, prog_bar=True)
+        self.log("train/mse", mse.detach(), batch_size=self.batch_size,
+                 sync_dist=True, prog_bar=False)
+        self.log("train/rmse", rmse.detach(), batch_size=self.batch_size,
+                 sync_dist=True, prog_bar=True)
+        self.log("train/mape", mape.detach(), batch_size=self.batch_size,
+                 sync_dist=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -177,26 +193,25 @@ class BaseModule(pl.LightningModule):
         if self.batch_size is None:
             self.batch_size = x.shape[0]
 
-        # MODIFIED FOR REVIN: compute denorm metrics, return denorm mse for val_loss
         if self.use_revin:
             y_hat = self.forward(x, y_old)
-            mse = self.mse(y_hat, y)
-            rmse = torch.sqrt(mse)
-            mape = self.mape(y_hat, y)
-            l1 = self.l1(y_hat, y)
         else:
-            y_hat = self.forward(x, y_old).reshape(-1)
+            y_hat = self.forward(x, y_old)
             y, y_hat = self.denormalize(y, y_hat)
-            mse = self.mse(y_hat, y)
-            rmse = torch.sqrt(mse)
-            mape = self.mape(y_hat, y)
-            l1 = self.l1(y_hat, y)
-        # END MODIFIED FOR REVIN
 
-        self.log("val/mse", mse.detach(), sync_dist=True, batch_size=self.batch_size, prog_bar=False)
-        self.log("val/rmse", rmse.detach(), sync_dist=True, batch_size=self.batch_size, prog_bar=True)
-        self.log("val/mape", mape.detach(), sync_dist=True, batch_size=self.batch_size, prog_bar=True)
-        self.log("val/mae", l1.detach(), batch_size=self.batch_size, sync_dist=True, prog_bar=False)
+        mse = self.mse(y_hat, y)
+        rmse = torch.sqrt(mse)
+        mape = self.mape(y_hat, y)
+        l1 = self.l1(y_hat, y)
+
+        self.log("val/mse", mse.detach(), sync_dist=True,
+                 batch_size=self.batch_size, prog_bar=False)
+        self.log("val/rmse", rmse.detach(), sync_dist=True,
+                 batch_size=self.batch_size, prog_bar=True)
+        self.log("val/mape", mape.detach(), sync_dist=True,
+                 batch_size=self.batch_size, prog_bar=True)
+        self.log("val/mae", l1.detach(), sync_dist=True,
+                 batch_size=self.batch_size, prog_bar=False)
         return {"val_loss": mse}
 
     def test_step(self, batch, batch_idx):
@@ -206,28 +221,30 @@ class BaseModule(pl.LightningModule):
         if self.batch_size is None:
             self.batch_size = x.shape[0]
 
-        # MODIFIED FOR REVIN: compute denorm metrics, return denorm mse for test_loss
         if self.use_revin:
             y_hat = self.forward(x, y_old)
-            mse = self.mse(y_hat, y)
-            rmse = torch.sqrt(mse)
-            mape = self.mape(y_hat, y)
-            l1 = self.l1(y_hat, y)
         else:
-            y_hat = self.forward(x, y_old).reshape(-1)
+            y_hat = self.forward(x, y_old)
             y, y_hat = self.denormalize(y, y_hat)
-            mse = self.mse(y_hat, y)
-            rmse = torch.sqrt(mse)
-            mape = self.mape(y_hat, y)
-            l1 = self.l1(y_hat, y)
-        # END MODIFIED FOR REVIN
 
-        self.log("test/mse", mse.detach(), sync_dist=True, batch_size=self.batch_size, prog_bar=False)
-        self.log("test/rmse", rmse.detach(), sync_dist=True, batch_size=self.batch_size, prog_bar=True)
-        self.log("test/mape", mape.detach(), sync_dist=True, batch_size=self.batch_size, prog_bar=True)
-        self.log("test/mae", l1.detach(), batch_size=self.batch_size, sync_dist=True, prog_bar=False)
+        mse = self.mse(y_hat, y)
+        rmse = torch.sqrt(mse)
+        mape = self.mape(y_hat, y)
+        l1 = self.l1(y_hat, y)
+
+        self.log("test/mse", mse.detach(), sync_dist=True,
+                 batch_size=self.batch_size, prog_bar=False)
+        self.log("test/rmse", rmse.detach(), sync_dist=True,
+                 batch_size=self.batch_size, prog_bar=True)
+        self.log("test/mape", mape.detach(), sync_dist=True,
+                 batch_size=self.batch_size, prog_bar=True)
+        self.log("test/mae", l1.detach(), sync_dist=True,
+                 batch_size=self.batch_size, prog_bar=False)
         return {"test_loss": mse}
 
+    # --------------------------------------------------------------------- #
+    #  optimizer
+    # --------------------------------------------------------------------- #
     def configure_optimizers(self):
         if self.optimizer == 'adam':
             optim = torch.optim.Adam(
@@ -239,9 +256,9 @@ class BaseModule(pl.LightningModule):
             )
         else:
             raise ValueError(f'Unimplemented optimizer {self.optimizer}')
-        scheduler = torch.optim.lr_scheduler.StepLR(optim, 
-                                                   self.lr_step_size, 
-                                                   self.lr_gamma)
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optim, self.lr_step_size, self.lr_gamma
+        )
         return [optim], [scheduler]
 
     def lr_scheduler_step(self, scheduler, *args, **kwargs):
