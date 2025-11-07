eddc76744d7ce78a34992fa53fdaadb0cb456e60
ToDoMoon <kewingchang@gmail.com>
2025-11-07 17:10:03 +0800

    Update save_to_drive.py

--- utils/save_to_drive.py
+++ utils/save_to_drive.py
@@ -5,8 +5,9 @@ from datetime import datetime
 
 if __name__ == "__main__":
 	# 生成时间戳
+	dir_time = datetime.now().strftime('%Y%m%d')
 	time_stamp = datetime.now().strftime('%Y%m%d_%H%M')
-	target_dir = f'/content/drive/MyDrive/training/{time_stamp}'
+	target_dir = f'/content/drive/MyDrive/training/{dir_time}/{time_stamp}'
 
 	# 创建目录
 	os.makedirs(target_dir, exist_ok=True)
@@ -28,7 +29,7 @@ if __name__ == "__main__":
 	print(f"Copied data/ to {target_dir}")
 	shutil.copytree('configs', os.path.join(target_dir, 'configs'), dirs_exist_ok=True)
 	print(f"Copied configs/ to {target_dir}")
-	shutil.copytree('Results', os.path.join(target_dir, 'results'), dirs_exist_ok=True)
-	print(f"Copied results/ to {target_dir}")
+	# shutil.copytree('Results', os.path.join(target_dir, 'results'), dirs_exist_ok=True)
+	# print(f"Copied results/ to {target_dir}")
 
 	print("Copy done.")
