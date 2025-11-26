6f73da9737cc0358004ddc8b8923bd81751fe36c
ToDoMoon <kewingchang@gmail.com>
2025-11-26 15:43:22 +0800

    Update extract_mase.py

--- scripts/extract_mase.py
+++ scripts/extract_mase.py
@@ -58,9 +58,9 @@ def main():
     # Define CSV columns
     columns = [
         'dir-name', 'additional_features',
-        'train/mae', 'train/mape', 'train/mse', 'train/rmse', 'train/focal', 'train/smooth_l1',
-        'val/mae', 'val/mape', 'val/mse', 'val/rmse', 'val/focal', 'val/smooth_l1',
-        'test/mae', 'test/mape', 'test/mse', 'test/rmse', 'test/focal', 'test/smooth_l1',
+        'train/mae', 'train/mape', 'train/mse', 'train/rmse', 'train/acc', 'train/smooth_l1',
+        'val/mae', 'val/mape', 'val/mse', 'val/rmse', 'val/acc', 'val/smooth_l1',
+        'test/mae', 'test/mape', 'test/mse', 'test/rmse', 'test/acc', 'test/smooth_l1',
         'note'
     ]
 
